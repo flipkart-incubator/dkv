61d734f3b9f9ea1bbe73051958858f7c64e4ed51
Kalyan Akella <kalyan.akella@gmail.com>
2020-05-13 14:31:34 +0530

    More tests around validation of iteration options

--- internal/server/storage/iter.go
+++ internal/server/storage/iter.go
@@ -8,12 +8,12 @@ import (
 	"github.com/flipkart-incubator/dkv/pkg/serverpb"
 )
 
+// IterationOptions captures the various options that can
+// be set to control iteration process.
 type IterationOptions interface {
 	KeyPrefix() ([]byte, bool)
 	StartKey() ([]byte, bool)
 }
-
-type IterationOption func(*iterOpts)
 
 type iterOpts struct {
 	keyPrefix []byte
@@ -41,6 +41,11 @@ func (io *iterOpts) validate() error {
 	return nil
 }
 
+// IterationOption captures an iteration option.
+type IterationOption func(*iterOpts)
+
+// NewIteratorOptions allows for the creation of `IterationOptions` instance
+// that is used for controlling the behavior of iterating through keyspace.
 func NewIteratorOptions(opts ...IterationOption) (IterationOptions, error) {
 	itOpts := new(iterOpts)
 	for _, opt := range opts {
@@ -50,18 +55,26 @@ func NewIteratorOptions(opts ...IterationOption) (IterationOptions, error) {
 	return itOpts, itOpts.validate()
 }
 
+// IterationPrefixKey is used to indicate the prefix of the keys
+// that are to be iterated. In other words, only those keys that
+// begin with this prefix are returned by the iterator.
 func IterationPrefixKey(prefix []byte) IterationOption {
 	return func(opts *iterOpts) {
 		opts.keyPrefix = prefix
 	}
 }
 
+// IterationStartKey sets the start key for iteration. All keys
+// starting with this key are returned by the iterator.
 func IterationStartKey(start []byte) IterationOption {
 	return func(opts *iterOpts) {
 		opts.startKey = start
 	}
 }
 
+// Iterator represents the behavior of a key space iterator
+// that allows for iterating though keys using the `HasNext`
+// and `Next` methods.
 type Iterator interface {
 	io.Closer
 	HasNext() bool
@@ -69,6 +82,9 @@ type Iterator interface {
 	Err() error
 }
 
+// Iteration is a convenience wrapper around `Iterator`
+// that allows for a given handler to be invoked exactly
+// once for every key value pair iterated.
 type Iteration interface {
 	ForEach(func([]byte, []byte) error) error
 }
@@ -93,6 +109,9 @@ func (iter *iteration) ForEach(hndlr func([]byte, []byte) error) error {
 	return itrtr.Err()
 }
 
+// NewIteration allows for the creation of an `Iteration` instance
+// that uses the underlying store's Iterator to callback for every
+// key value pair iterated.
 func NewIteration(kvs KVStore, iterReq *serverpb.IterateRequest) Iteration {
 	itOpts := &iterOpts{iterReq.KeyPrefix, iterReq.StartKey}
 	return &iteration{kvs, itOpts}
---
+++ internal/server/storage/iter_test.go
@@ -0,0 +1,45 @@
+package storage
+
+import "testing"
+
+func TestIterationOptionsValidation(t *testing.T) {
+	itOps := new(iterOpts)
+	if _, present := itOps.KeyPrefix(); present {
+		t.Errorf("Expected no key prefix to be set")
+	}
+
+	if _, present := itOps.StartKey(); present {
+		t.Errorf("Expected no start key to be set")
+	}
+
+	expKeyPrefix := "pref"
+	itOps.keyPrefix = []byte(expKeyPrefix)
+	if kp, present := itOps.KeyPrefix(); !present || string(kp) != expKeyPrefix {
+		t.Errorf("Expected key prefix to be set and equal to %s", expKeyPrefix)
+	}
+
+	if err := itOps.validate(); err == nil {
+		t.Errorf("Expected validation errors but got none")
+	} else {
+		t.Log(err)
+	}
+
+	expStartKey := "start"
+	itOps.startKey = []byte(expStartKey)
+	if sk, present := itOps.StartKey(); !present || string(sk) != expStartKey {
+		t.Errorf("Expected start key to be set and equal to %s", expStartKey)
+	}
+
+	if err := itOps.validate(); err == nil {
+		t.Errorf("Expected validation errors but got none")
+	} else {
+		t.Log(err)
+	}
+
+	expStartKey = expKeyPrefix + expStartKey
+	itOps.startKey = []byte(expStartKey)
+
+	if err := itOps.validate(); err != nil {
+		t.Errorf("Expected no validation error. But got error: %v", err)
+	}
+}
