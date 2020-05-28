7005b2c77b0cd4965e20c7e4f825c8ad49dd0cae
Kalyan Akella <kalyan.akella@gmail.com>
2020-05-28 18:52:10 +0530

    Adding the basic folder and project structure for DKV Java client

---
+++ clients/java/dkv-client/src/main/java/org/dkv/client/App.java
@@ -0,0 +1,13 @@
+package org.dkv.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
