1f4355d40b8a00cbd8485562899f9c5ddfc9e35f
Kalyan Akella <kalyan.akella@gmail.com>
2020-07-06 08:44:02 +0530

    Wrapping up Jepsen tests with minor refactoring

--- extras/jepsen-dkv/src/jepsen/dkv.clj
+++ extras/jepsen-dkv/src/jepsen/dkv.clj
@@ -10,6 +10,7 @@
      [client :as client]
      [db :as db]
      [generator :as gen]
+     [independent :as independent]
      [nemesis :as nemesis]
      [tests :as tests]]
     [knossos.model :as model]
@@ -49,6 +50,18 @@
   [s]
   (when (and s (not (empty? (.trim s)))) (Long/parseLong (.trim s))))
 
+(def cli-opts
+  "Additional command line options."
+  [["-q" "--quorum" "Use quorum reads, instead of reading from any replica."]
+   [nil "--qps NUM" "Approximate number of requests per second, per thread."
+    :default 10
+    :parse-fn read-string
+    :validate [#(and (number? %) (pos? %)) "Must be a positive number"]]
+   [nil "--ops-per-key NUM" "Maximum number of operations on any given key."
+    :default  50
+    :parse-fn parse-long
+    :validate [pos? "Must be a positive integer."]]])
+
 (defn db
   "DKV DB for a particular version."
   [version]
@@ -87,14 +100,15 @@
   (setup! [this test])
 
   (invoke! [_ test op]
+    (let [[k v] (:value op)]
     (try+
       (case (:f op)
-        :read (let [value (-> conn (dkvcli/getValue "jepsen_key") parse-long)]
-                (assoc op :type :ok, :value value))
-        :write (do (dkvcli/putKV conn "jepsen_key" (str (:value op)))
+        :read (let [value (-> conn (dkvcli/getValue (:quorum test) k) parse-long)]
+                (assoc op :type :ok, :value (independent/tuple k value)))
+        :write (do (dkvcli/putKV conn k (str v))
                    (assoc op :type :ok)))
       (catch java.lang.Exception ex
-        (assoc op :type (if (= :read (:f op)) :fail :info) :error :timeout))))
+        (assoc op :type (if (= :read (:f op)) :fail :info) :error :timeout)))))
 
   (teardown! [this test])
 
@@ -105,27 +119,33 @@
 
 (defn dkv-test
   [opts]
-  (merge tests/noop-test opts
-         {:name "dkv"
-          :os debian/os
-          :db (db "1.0.0")
-          :client (Client. nil)
-          :nemesis (nemesis/partition-random-halves)
-          :checker (checker/compose
-                     {:perf   (checker/perf)
-                      :linear (checker/linearizable {:model     (model/register)
-                                                     :algorithm :linear})
-                      :timeline (timeline/html)})
-          :generator (->> (gen/mix [r w])
-                          (gen/stagger 1)
-                          (gen/nemesis
-                            (gen/seq (cycle [(gen/sleep 5)
-                                             {:type :info, :f :start}
-                                             (gen/sleep 5)
-                                             {:type :info, :f :stop}])))
-                          (gen/time-limit (:time-limit opts)))}))
+  (let [quorum (boolean (:quorum opts))]
+    (merge tests/noop-test opts
+           {:name "dkv"
+            :quorum quorum
+            :os debian/os
+            :db (db "1.0.0")
+            :client (Client. nil)
+            :nemesis (nemesis/partition-random-halves)
+            :checker (checker/compose
+                       {:perf (checker/perf)
+                        :indep (independent/checker
+                                 (checker/compose
+                                   {:linear (checker/linearizable {:model (model/register) :algorithm :linear})
+                                    :timeline (timeline/html)}))})
+            :generator (->> (independent/concurrent-generator (:concurrency opts) (range)
+                                                              (fn [k] (->>
+                                                                        (gen/mix [r w])
+                                                                        (gen/stagger (/ (:qps opts)))
+                                                                        (gen/limit (:ops-per-key opts)))))
+                            (gen/nemesis
+                              (gen/seq (cycle [(gen/sleep 5)
+                                               {:type :info, :f :start}
+                                               (gen/sleep 5)
+                                               {:type :info, :f :stop}])))
+                            (gen/time-limit (:time-limit opts)))})))
 
 (defn -main
   [& args]
-  (cli/run! (merge (cli/single-test-cmd {:test-fn dkv-test}) 
+  (cli/run! (merge (cli/single-test-cmd {:test-fn dkv-test :opt-spec cli-opts})
                    (cli/serve-cmd)) args))
