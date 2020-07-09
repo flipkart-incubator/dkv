fe531c23d8e5e5a2f98f283e56add50c903f591c
Kalyan Akella <kalyan.akella@gmail.com>
2020-07-09 22:07:31 +0530

    Adding the core benchmark logic as well as their unit tests

--- extras/YCSB/dkv/src/main/java/site/ycsb/db/DKVClient.java
+++ extras/YCSB/dkv/src/main/java/site/ycsb/db/DKVClient.java
@@ -1,48 +1,148 @@
 package site.ycsb.db;
 
-import site.ycsb.ByteIterator;
-import site.ycsb.DB;
-import site.ycsb.DBException;
-import site.ycsb.Status;
+import dkv.serverpb.Api;
+import org.dkv.client.DKVEntry;
+import site.ycsb.*;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
+
+import static java.lang.String.format;
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class DKVClient extends DB {
-  @Override
-  public void init() throws DBException {
-    super.init();
-  }
+  private static final int DEFAULT_PORT = 8080;
+  private static final String DEFAULT_HOST = "127.0.0.1";
+  private static final String HOST_PROPERTY = "dkv.host";
+  private static final String PORT_PROPERTY = "dkv.port";
+  private static final String PRIMARY_KEY = "@@@PRIMARY@@@";
+
+  private org.dkv.client.DKVClient dkvClient;
 
   @Override
-  public void cleanup() throws DBException {
-    super.cleanup();
+  public void init() {
+    Properties props = getProperties();
+
+    String host = props.getProperty(HOST_PROPERTY);
+    if (host == null || host.trim().isEmpty()) {
+      host = DEFAULT_HOST;
+    }
+
+    int port = DEFAULT_PORT;
+    String portString = props.getProperty(PORT_PROPERTY);
+    if (portString != null && !portString.trim().isEmpty()) {
+      port = Integer.parseInt(portString);
+    }
+    dkvClient = new org.dkv.client.DKVClientImpl(host, port);
   }
 
   @Override
   public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
-    return null;
+    try {
+      if (fields == null || fields.isEmpty()) {
+        byte[] startKey = toDKVKey(table, key, PRIMARY_KEY);
+        byte[] keyPrefix = toDKVKey(table, key, "");
+        Iterator<DKVEntry> iter = dkvClient.iterate(startKey, keyPrefix);
+        while (iter.hasNext()) {
+          DKVEntry entry = iter.next();
+          entry.checkStatus();
+          String currField = fromDKVKey(entry.getKeyAsString())[2];
+          if (!PRIMARY_KEY.equals(currField)) {
+            result.put(currField, new ByteArrayByteIterator(entry.getValueAsByteArray()));
+          }
+        }
+      } else {
+        String[] flds = fields.toArray(new String[0]);
+        byte[][] keys = new byte[flds.length][];
+        for (int i = 0; i < flds.length; i++) {
+          keys[i] = toDKVKey(table, key, flds[i]);
+        }
+        byte[][] values = dkvClient.multiGet(Api.ReadConsistency.LINEARIZABLE, keys);
+        for (int i = 0; i < flds.length; i++) {
+          result.put(flds[i], new ByteArrayByteIterator(values[i]));
+        }
+      }
+      return Status.OK;
+    } catch (Exception e) {
+      return handleErrStatus(e);
+    }
   }
 
   @Override
   public Status scan(String table, String startkey, int recordcount, Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-    return null;
+    try {
+      byte[] startKeyBytes = toDKVKey(table, startkey, PRIMARY_KEY);
+      Iterator<DKVEntry> itrtr = dkvClient.iterate(startKeyBytes);
+      result.add(new LinkedHashMap<>());
+      String prevKey = startkey;
+      while (itrtr.hasNext()) {
+        DKVEntry entry = itrtr.next();
+        entry.checkStatus();
+        String[] comps = fromDKVKey(entry.getKeyAsString());
+        String currKey = comps[1], currField = comps[2];
+        if (currKey.equals(prevKey)) {
+          if (!PRIMARY_KEY.equals(currField) && (fields == null || fields.isEmpty() || fields.contains(currField))) {
+            result.lastElement().put(currField, new ByteArrayByteIterator(entry.getValueAsByteArray()));
+          }
+        } else {
+          prevKey = currKey;
+          recordcount--;
+          if (recordcount <= 0) {
+            break;
+          }
+          result.add(new LinkedHashMap<>());
+        }
+      }
+      return Status.OK;
+    } catch (Exception e) {
+      return handleErrStatus(e);
+    }
   }
 
   @Override
   public Status update(String table, String key, Map<String, ByteIterator> values) {
-    return null;
+    try {
+      dkvClient.put(toDKVKey(table, key, PRIMARY_KEY), key.getBytes(UTF_8));
+      for (Map.Entry<String, ByteIterator> entry : values.entrySet()) {
+        String field = entry.getKey();
+        byte[] dkvKeyBytes = toDKVKey(table, key, field);
+        byte[] dkvValueBytes = entry.getValue().toArray();
+        dkvClient.put(dkvKeyBytes, dkvValueBytes);
+      }
+      return Status.OK;
+    } catch (Exception e) {
+      return handleErrStatus(e);
+    }
   }
 
   @Override
   public Status insert(String table, String key, Map<String, ByteIterator> values) {
-    return null;
+    return this.update(table, key, values);
   }
 
   @Override
   public Status delete(String table, String key) {
-    return null;
+    throw new UnsupportedOperationException("Delete not implemented in DKV");
+  }
+
+  private Status handleErrStatus(Exception e) {
+    System.err.println(e.getMessage());
+    e.printStackTrace();
+    return Status.ERROR;
+  }
+
+  private String[] fromDKVKey(String entryKey) {
+    String[] firstComps = entryKey.split("_");
+    if (firstComps.length != 2) {
+      throw new IllegalArgumentException(format("Invalid DKV key supplied: %s", entryKey));
+    }
+    String[] secondComps = firstComps[1].split(":");
+    if (secondComps.length != 2) {
+      throw new IllegalArgumentException(format("Invalid DKV key supplied: %s", entryKey));
+    }
+    return new String[] {/* table */ firstComps[0], /* key */ secondComps[0], /* field */ secondComps[1]};
+  }
+
+  private byte[] toDKVKey(String table, String key, String field) {
+    return format("%s_%s:%s", table, key, field).getBytes(UTF_8);
   }
 }
---
+++ extras/YCSB/dkv/src/test/java/site/ycsb/db/DKVClientTest.java
@@ -0,0 +1,90 @@
+package site.ycsb.db;
+
+import org.junit.Before;
+import org.junit.Test;
+import site.ycsb.ByteIterator;
+import site.ycsb.Status;
+import site.ycsb.StringByteIterator;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import static org.junit.Assert.*;
+
+public class DKVClientTest {
+    private static final String TEST_TABLE = "customers";
+    private DKVClient dkvClient;
+
+    @Before
+    public void setup() {
+        dkvClient = new DKVClient();
+        dkvClient.init();
+    }
+
+    @Test
+    public void shouldInsertAndReadData() {
+        Map<String, String> fv1 = fieldValues("name", "Cust1", "age", "21", "pincode", "560001");
+        assertEquals(Status.OK, dkvClient.insert(TEST_TABLE, "1111", transform(fv1)));
+        LinkedHashMap<String, ByteIterator> result1 = new LinkedHashMap<>();
+        assertEquals(Status.OK, dkvClient.read(TEST_TABLE, "1111", fv1.keySet(), result1));
+        assertResults(result1, fv1);
+
+        Map<String, String> fv2 = fieldValues("name", "Cust2", "age", "22", "pincode", "560002");
+        assertEquals(Status.OK, dkvClient.insert(TEST_TABLE, "2222", transform(fv2)));
+        LinkedHashMap<String, ByteIterator> result2 = new LinkedHashMap<>();
+        fv2.remove("age");      // read only "name" and "pincode" fields
+        assertEquals(Status.OK, dkvClient.read(TEST_TABLE, "2222", fv2.keySet(), result2));
+        assertResults(result2, fv2);
+        fv2.put("age", "22");       // restore "age" field
+
+        Map<String, String> fv3 = fieldValues("name", "Cust3", "age", "23", "pincode", "560003");
+        assertEquals(Status.OK, dkvClient.insert(TEST_TABLE, "3333", transform(fv3)));
+        LinkedHashMap<String, ByteIterator> result3 = new LinkedHashMap<>();
+        // read all fields
+        assertEquals(Status.OK, dkvClient.read(TEST_TABLE, "3333", null, result3));
+        assertResults(result3, fv3);
+
+        Vector<HashMap<String, ByteIterator>> results = new Vector<>();
+        int recordcount = 2;
+        assertEquals(Status.OK, dkvClient.scan(TEST_TABLE, "1111", recordcount, null, results));
+        assertEquals(recordcount, results.size());
+        assertResults(results.get(0), fv1);
+        assertResults(results.get(1), fv2);
+
+        results = new Vector<>();
+        recordcount = 5;
+        assertEquals(Status.OK, dkvClient.scan(TEST_TABLE, "1111", recordcount, null, results));
+        assertEquals(3, results.size());
+        assertResults(results.get(0), fv1);
+        assertResults(results.get(1), fv2);
+        assertResults(results.get(2), fv3);
+    }
+
+    private Map<String, ByteIterator> transform(Map<String, String> fvs) {
+        LinkedHashMap<String, ByteIterator> results = new LinkedHashMap<>();
+        for (Map.Entry<String, String> entry : fvs.entrySet()) {
+            results.put(entry.getKey(), new StringByteIterator(entry.getValue()));
+        }
+        return results;
+    }
+
+    private void assertResults(Map<String, ByteIterator> results, Map<String, String> fieldVals) {
+        assertEquals(fieldVals.size(), results.size());
+        for (Map.Entry<String, String> entry : fieldVals.entrySet()) {
+            String expKey = entry.getKey();
+            String expVal = entry.getValue();
+            assertEquals(expVal, results.get(expKey).toString());
+        }
+    }
+
+    private Map<String, String> fieldValues(String key, String val, String... kvs) {
+        LinkedHashMap<String, String> result = new LinkedHashMap<>();
+        result.put(key, val);
+        for (int i = 0; i < (kvs.length-1); i+=2) {
+            result.put(kvs[i], kvs[i+1]);
+        }
+        return result;
+    }
+}
