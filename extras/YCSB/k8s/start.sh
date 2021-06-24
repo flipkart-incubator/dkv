2399ff56fd00f06cfb64733707c57ee93fa5b248
kinshuk.bairagi <me@kinshuk.in>
2021-06-24 12:13:28 +0530

    update dockerfile

--- extras/YCSB/k8s/start.sh
+++ extras/YCSB/k8s/start.sh
@@ -5,24 +5,32 @@ set -e
 function load_data
 {
     if [[ ! -e /.loaded_data ]]; then
-        /opt/ycsb-*/bin/ycsb.sh load "${DBTYPE}" -s -P "workloads/workload${WORKLETTER}" "${DBARGS}" && touch /.loaded_data
+        /opt/ycsb-*/bin/ycsb.sh load "${DBTYPE}" -s -P "${WORKLOAD}" "${DBARGS}"
     fi
     return
 }
 
 # exit message
-trap 'echo "ycsb-run has finished"' EXIT
+# trap 'echo "ycsb-run has finished"' EXIT
+
+JVM_ARGUMENTS="${JVM_ARGS:-""}"
+THREADS="${NUM_THREADS:-"1"}"
 
 
 # make sure all the params are set and go.
-if [[ -z ${DBTYPE} || -z ${WORKLETTER} || -z ${DBARGS} ]]; then
-  echo "Missing params! Exiting"
-  exit 1
+if [[ -z ${DBTYPE} || -z ${WORKLOAD} || -z ${DBARGS} ]]; then
+    echo "Missing params! Exiting"
+    exit 1
 else
-  if [[ ! -z "${ACTION}" ]]; then
-    eval ./bin/ycsb "${ACTION}" "${DBTYPE}" -s -P "workloads/workload${WORKLETTER}" "${DBARGS}"
-  else
-    load_data
-    eval ./bin/ycsb run "${DBTYPE}" -s -P "workloads/workload${WORKLETTER}" "${DBARGS}"
-  fi
+    echo "Running WORKLOAD"
+    cat "${WORKLOAD}"
+
+    if [[ ! -z "${ACTION}" ]]; then
+        eval ./bin/ycsb "${ACTION}" "${DBTYPE}" -s -P "${WORKLOAD}" "${DBARGS}" -jvm-args "${JVM_ARGUMENTS}" -threads "${THREADS}"
+    else
+        load_data
+        eval ./bin/ycsb run "${DBTYPE}" -s -P "${WORKLOAD}" "${DBARGS}" -jvm-args "${JVM_ARGUMENTS}" -threads "${THREADS}"
+    fi
+
+    sleep infinity
 fi
